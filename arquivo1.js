a35eda103b06872cb6d5af185cd6cbae3bd2856b
Camila Aparecida de Souza <camilaaparecida593@gmail.com>
2021-03-08 22:33:36 -0300

    Alterando os arquivos 1 e 2

--- arquivo1.js
+++ arquivo1.js
@@ -1,1 +1,2 @@
 console.log('Este é o Arquivo 1');
+console.log('Adicionando conteúdo ao Arquivo 1');
--- arquivo2.js
+++ arquivo2.js
@@ -1,1 +1,2 @@
 console.log('Este é o Arquivo 2');
+console.log('Adicionando conteúdo ao Arquivo 2');
